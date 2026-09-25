8fa612f9e6ce173c1f88732f42ff90b74de5e871
Tyger Moth <146899581+TygerMoth@users.noreply.github.com>
2026-09-25 11:44:12 -0400

    Enhance ButtonWidget with state management and redraw
    Enhanced ButtonWidget to support enabled, visible, and smooth states with corresponding methods. Modified label position calculation and added redraw functionality.

--- src/widgets/button/ButtonWidget.cpp
+++ src/widgets/button/ButtonWidget.cpp
@@ -3,6 +3,12 @@
 /***************************************************************************************
 ** Code for the GFX button UI element
 ** Grabbed from Adafruit_tft library and enhanced to handle any label font
+****************************************************************************************
+*** Modified label datum calculation of _yd to allow button heights as small as 12 px
+*** Added Enabled, Visible and Smooth states,
+*** with enable(), disable(), show(), hide() and added redraw() methods,
+*** and modification to contains() method to process only enabled and visible buttons.
+*** Modified Sep 23, 2026 - Larry Coffey
 ***************************************************************************************/
 ButtonWidget::ButtonWidget(TFT_eSPI *tft) {
   _tft       = tft;
@@ -10,8 +16,11 @@ ButtonWidget::ButtonWidget(TFT_eSPI *tft) {
   _yd        = 0;
   _textdatum = MC_DATUM;
   _label[9]  = '\0';
-  _currstate  = false;
-  _laststate  = false;
+  _currstate = false;
+  _laststate = false;
+  _enblstate = false;
+  _vsblstate = false;
+  _smthstate = true;
   _inverted  = false;
 }
 
@@ -50,11 +59,10 @@ void ButtonWidget::initButtonUL(int16_t x1, int16_t y1, uint16_t w, uint16_t h, 
 }
 
 // Adjust text datum and x, y deltas
-void ButtonWidget::setLabelDatum(int16_t x_delta, int16_t y_delta, uint8_t datum)
-{
+void ButtonWidget::setLabelDatum(int16_t x_delta, int16_t y_delta, uint8_t datum) {
   int16_t _yd_min = 5; // added int16_t vartiable _yd_min = 5, for minimum of 12 px height buttons
   _xd        = x_delta;
-  _yd        = max(y_delta, _yd_min); // changed from y_delta to max(y_delta, _yd_min)
+  _yd        = max(y_delta, _yd_min); // changed from y_delta to max(y_delta, _yd_min) Sept 2026 LC
   _textdatum = datum;
 }
 
@@ -94,13 +102,14 @@ void ButtonWidget::drawButton(bool inverted, String long_name) {
     _tft->setTextPadding(0);
 
     if (long_name == "")
-      _tft->drawString(_label, _x1 + (_w/2) + _xd, _y1 + (_h/2) + _yd); // changed from (_h/2) - 4 + _yd to (_h/2) - 4 + _yd
+      _tft->drawString(_label, _x1 + (_w/2) + _xd, _y1 + (_h/2) + _yd); // changed from (_h/2) - 4 + _yd to (_h/2) + _yd
     else
       _tft->drawString(long_name, _x1 + (_w/2) + _xd, _y1 + (_h/2) + _yd); // after the change above to _yd = min(..., 5)
 
     _tft->setTextDatum(tempdatum);
     _tft->setTextPadding(tempPadding);
   }
+  _smthstate = false; // added Sept 26 LC
 }
 
 void ButtonWidget::drawSmoothButton(bool inverted, int16_t outlinewidth, uint32_t bgcolor, String long_name) {
@@ -140,23 +149,71 @@ void ButtonWidget::drawSmoothButton(bool inverted, int16_t outlinewidth, uint32_
     _tft->setTextPadding(0);
 
     if (long_name == "")
-      _tft->drawString(_label, _x1 + (_w/2) + _xd, _y1 + (_h/2) + _yd); // changed from (_h/2) - 4 + _yd to (_h/2) - 4 + _yd
+      _tft->drawString(_label, _x1 + (_w/2) + _xd, _y1 + (_h/2) + _yd); // changed from (_h/2) - 4 + _yd to (_h/2) + _yd
     else
       _tft->drawString(long_name, _x1 + (_w/2) + _xd, _y1 + (_h/2) + _yd); // after the change above to _yd = min(..., 5)
 
     _tft->setTextDatum(tempdatum);
     _tft->setTextPadding(tempPadding);
   }
+  _smthstate = true; // added Sept 26 LC
+}
+// added Sept 2026 LC
+void ButtonWidget::redraw() {
+  if(_smthstate) drawSmoothButton(_inverted, _outlinewidth, _bgcolor, _label);
+  else drawButton(_inverted, _label);
+}
+
+void ButtonWidget::disable() { // Disable button (prevent interaction)
+  _enblstate = false;
+  redrawDisabled();
+}
+
+void ButtonWidget::enable() { // Enable button (allow interaction)
+  _enblstate = true;
+  redraw();
+}
+
+void ButtonWidget::show() { // Show button (make visible)
+  _vsblstate = true;
+  redraw();
+}
+
+void ButtonWidget::hide() { // Hide button (make invisible)
+  _vsblstate = false;
+  erase();
+}
+
+void ButtonWidget::erase() {  // Erase button
+  _tft->fillRect(_x1, _y1, _w, _h, _bgcolor);
+}
+
+void ButtonWidget::toggleEnabled() {  // Toggle enable/disable
+  _enblstate = !_enblstate;
+  redraw();
+}
+
+void ButtonWidget::toggleVisible() {  // Toggle visible/hidden
+  _vsblstate = !_vsblstate;
+  if (_vsblstate) redraw();
+  else erase();
+}
+
+void ButtonWidget::redrawDisabled() {  // Draw disabled appearance
+    if (_vsblstate) redrawDisabledInternal();
 }
 
 bool ButtonWidget::contains(int16_t x, int16_t y) {
+  if (!_vsblstate || !_enblstate) return false; // added Sept 2026
   return ((x >= _x1) && (x < (_x1 + _w)) &&
           (y >= _y1) && (y < (_y1 + _h)));
 }
 
-void ButtonWidget::press(bool p) {
-  _laststate = _currstate;
-  _currstate = p;
+void ButtonWidget::press(bool p) { // modified Sept 2026 LC
+  if (_vsblstate && _enblstate) { // ignore press(T/F) if not visible or enabled
+    _laststate = _currstate;
+    _currstate = p;
+  }
 }
 
 bool ButtonWidget::isPressed()    { return _currstate; }
